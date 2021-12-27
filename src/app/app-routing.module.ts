63363f98ff6eb0d6b6bab5bc8c613e945b7a366b
Tharanga_Dilshan <59679693+TharangaDilshan403@users.noreply.github.com>
2021-12-27 08:01:26 +0530

    auth service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LogInComponent } from './log-in/log-in.component';
+import { LogInComponent } from './components/log-in/log-in.component';
 
 const routes: Routes = [
   {path:'',component:LogInComponent}
